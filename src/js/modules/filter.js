980d06a36441d6bdefb925e4b3cad6c3eb1260ea
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-11-07 00:34:26 -0500

    hook up the count

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -12,24 +12,52 @@ module.exports =  {
         }.bind(this));
     },
 
+    filterBy: function(target) {
+        var $target = $(target);
+
+        if ($target.hasClass('is-selected')) {
+            $target.removeClass('is-selected');
+        } else {
+            $target.parent().find('.is-selected').removeClass('is-selected');
+            $target.addClass('is-selected');
+        }
+
+        this.setSettings();
+    },
+
     setSettings: function() {
         $('.uit-filter-set').each(function(i, el) {
             var settingName = $(el).data('set');
             var selected = $(el).find('.is-selected').text();
 
-            
-
             settings[settingName] = selected;
         }.bind(this));
 
-        console.log(settings);
+        this.filterStates();
     },
 
-    filterBy: function(target) {
-        var $target = $(target);
-        $target.parent().find('.is-selected').removeClass('is-selected');
-        $target.addClass('is-selected');
+    filterStates: function() {
+        $('.is-filtered').removeClass('is-filtered');
+        $('.uit-map path').each(function(i, el) {
 
-        this.setSettings();
+            for (var i in settings) {
+                if ($(el) && $(el)[0].hasAttribute('data-' + i)) {
+                    if (settings[i] !== "" && settings[i] !== $(el).data(i)) {
+                        $(el).addClass('is-filtered');
+                    }
+                } else {
+                    // this can be removed when data is complete
+                    $(el).addClass('is-filtered');
+                }
+            }
+        }.bind(this));
+
+        this.updateCount();
+    },
+
+    updateCount: function() {
+        var count = 537 - ($('.is-filtered').length + 1);
+
+        $('.uit-count__count').text(count);
     }
 };
