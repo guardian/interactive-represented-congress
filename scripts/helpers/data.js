16cf572622de2d9ece17b5061936c065974bf534
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-11-06 23:48:19 -0500

    create the map

--- scripts/helpers/data.js
+++ scripts/helpers/data.js
@@ -4,12 +4,14 @@ var gsjson = require('google-spreadsheet-to-json');
 var deasync = require('deasync');
 var request = require('sync-request');
 var csvjson = require('csvjson');
+var cheerio = require('cheerio');
 
 var config = require('../config.json');
 var userHome = require('user-home');
 var keys = require(userHome + '/.gu/interactives.json');
 
 var data;
+var $;
 
 function fetchData(callback) {
     gsjson({
@@ -66,6 +68,34 @@ function compileChamber(chamber) {
     return compiledSeats;
 }
 
+function calculateAgeRange(age) {
+    if (age >= 65) {
+        return 'Over 65'
+    } else if (age >= 45) {
+        return '45-64'
+    } else {
+        return 'Under 45'
+    }
+}
+
+function mapData(chamber) {
+    for (var i in data.compiled[chamber]) {
+        var seat = data.compiled[chamber][i];
+        var $seat = $('#' + (chamber === 'senate' ? 'SN_' : '') +  seat.seat);
+
+        if ($seat.length > 0) {
+            $seat.attr('data-gender', seat.gender);
+            $seat.attr('data-ethnicity', seat.ethnicity);
+            $seat.attr('data-age', calculateAgeRange(seat.age));
+            $seat.attr('data-religion', seat.religion);
+            $seat.attr('data-orientation', seat.orientation === 'straight' ? 'straight' : 'LGB');
+        } else {
+            console.log('Can\'t find ' + seat.seat + ' in map');
+        }
+
+    }
+}
+
 function getData() {
     var isDone = false;
 
@@ -89,6 +119,14 @@ function getData() {
         data.compiled.senate = compileChamber('senate');
         console.log('buildling house demographics list...');
         data.compiled.house = compileChamber('house');
+
+        var map = fs.readFileSync('./src/templates/partials/map.html');
+            $ = cheerio.load(map);
+
+        mapData('house');
+        mapData('senate');
+
+        fs.writeFileSync('./src/templates/partials/map-compiled.html', $.html());
 
         fs.mkdirsSync('./.data');
         fs.writeFileSync('./.data/data.json', JSON.stringify(data.compiled));
