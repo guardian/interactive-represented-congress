7ade4acf66abd4c07649d8773bd28501e475ba06
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-11-09 10:41:07 -0500

    get real seat names on hover state

--- src/js/modules/hover.js
+++ src/js/modules/hover.js
@@ -1,4 +1,5 @@
 var mapPosition;
+var codeToNameDict = {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
 
 module.exports =  {
     init: function() {
@@ -28,7 +29,7 @@ module.exports =  {
         $('.uit-map__tooltip-name').text(data.name);
         $('.uit-map__tooltip-party').addClass('is-' + data.party);
         $('.uit-map__tooltip-party').text('(' + data.party + ')');
-        $('.uit-map__tooltip-seat').text($(el).attr('id'));
+        $('.uit-map__tooltip-seat').text(this.formatSeat($(el).attr('id')));
 
         var pointPosition = $(el).offset();
 
@@ -43,5 +44,38 @@ module.exports =  {
             $('.uit-map__tooltip-party').removeClass().addClass('uit-map__tooltip-party');
             $('.uit-map__tooltip').removeClass('is-visible');
         });
+    },
+
+    formatSeat: function(id) {
+        if (id.includes('SN_')) {
+            var isSenate = true;
+            id = id.replace('SN_', '');
+        }
+
+        var state = codeToNameDict[id.substring(0, 2)];
+
+        if (isSenate) {
+            return 'Senate ' + state;
+        } else {
+            var number = parseInt(id.substring(2));
+                number = this.suffixise(number);
+
+            return 'House ' + state + ' ' + number;
+        }
+    },
+
+    suffixise: function(i) {
+        var j = i % 10,
+        k = i % 100;
+        if (j == 1 && k != 11) {
+            return i + "st";
+        }
+        if (j == 2 && k != 12) {
+            return i + "nd";
+        }
+        if (j == 3 && k != 13) {
+            return i + "rd";
+        }
+        return i + "th";
     }
 };
